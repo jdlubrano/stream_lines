2fd2387a632d79323cd930bc50020fd4f5f5d361
Joel Lubrano <joel.lubrano@stitchfix.com>
2020-02-13 16:50:22 -0500

    Add first-class support for streaming JSON lines

---
+++ lib/stream_lines/reading/json_lines.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'stream_lines/reading/stream'
+
+module StreamLines
+  module Reading
+    class JSONLines
+      include Enumerable
+
+      def initialize(url, **json_options)
+        @url = url
+        @json_options = json_options
+        @stream = Stream.new(url)
+      end
+
+      def each(&block)
+        @stream.each { |line| block.call(parse_line(line)) }
+      end
+
+      private
+
+      attr_reader :url
+
+      def parse_line(line)
+        JSON.parse(line, **@json_options)
+      end
+    end
+  end
+end
