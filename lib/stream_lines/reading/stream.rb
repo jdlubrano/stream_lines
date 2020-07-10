4d267b01e814c92b2672b913275825855f994ecd
Joel Lubrano <joel.lubrano@stitchfix.com>
2020-07-10 14:57:29 -0400

    Withold invalid byte sequences and hope that prepending them to the next chunk makes valid bytes sequences

--- lib/stream_lines/reading/stream.rb
+++ lib/stream_lines/reading/stream.rb
@@ -39,16 +39,29 @@ module StreamLines
       end
 
       def extract_lines(chunk)
-        encoded_chunk = chunk.to_s.dup.force_encoding(@encoding)
-        lines = encoded_chunk.split($INPUT_RECORD_SEPARATOR, -1)
-
-        if lines.length > 1
-          lines.first.prepend(@buffer)
-          @buffer = String.new(encoding: @encoding)
-        end
-
+        encoded_chunk = @buffer + chunk.to_s.dup.force_encoding(@encoding)
+        lines = split_lines(encoded_chunk)
+        @buffer = String.new(encoding: @encoding)
         @buffer << lines.pop.to_s
+
         lines
+      end
+
+      def split_lines(encoded_chunk)
+        lines = encoded_chunk.split($INPUT_RECORD_SEPARATOR, -1)
+      rescue ArgumentError => e
+        raise e unless /invalid byte sequence/.match?(e.message)
+
+        # NOTE: (jdlubrano)
+        # The last byte in the chunk is most likely a part of a multibyte
+        # character that, on its own, is an invalid byte sequence.  So, we
+        # want to split the lines containing all valid bytes and make the
+        # trailing bytes the last line.  The last line eventually gets added
+        # to the buffer, prepended to the next chunk, and, hopefully, restores
+        # a valid byte sequence.
+        last_newline_index = encoded_chunk.rindex($INPUT_RECORD_SEPARATOR)
+        valid_lines = encoded_chunk[0...last_newline_index].split($INPUT_RECORD_SEPARATOR, -1)
+        valid_lines + [encoded_chunk[(last_newline_index + 1)..-1]].compact
       end
     end
   end
